ef69f057360d1791b1d24e7659464281ef93013d
txipi <txipi@lt-garaizar>
2026-03-06 09:58:24 +0100

    DeustoAir terminado

--- prog2-2026/src/poo/deustoair/DeustoAir.java
+++ prog2-2026/src/poo/deustoair/DeustoAir.java
@@ -76,11 +76,40 @@ public class DeustoAir {
 	 * buscarVuelosAeronave: método estático que recibe una lista de vuelos y una aeronave y devuelve
 	 * una lista vuelos con los vuelos que tengan la aeronave que nos han pasado 
 	 */
+	public static ArrayList<Vuelo> buscarVuelosAeronave (ArrayList<Vuelo> vuelos, Aeronave aeronave) {
+		ArrayList<Vuelo> resultado = new ArrayList<Vuelo>();
+
+		for (Vuelo vuelo : vuelos) {
+			if (vuelo.getAeronave().equals(aeronave)) {
+				resultado.add(vuelo);
+			}
+		}
+		
+		return resultado;
+	}
+	
 	
 	/*
 	 * mayorAeronave: método estático que recibe una lista de vuelos y devuelve
 	 * la aeronave que más se ha utilizado en todos esos vuelos
 	 */
+	
+	public static Aeronave mayorAeronave (ArrayList<Vuelo> vuelos) {
+		Aeronave mayor = vuelos.get(0).getAeronave();
+		
+		for (Vuelo vuelo : vuelos) {
+			// Busco los vuelos que se vuelan con la aeronave "mayor"
+			ArrayList<Vuelo> vuelosMayor = buscarVuelosAeronave(vuelos, mayor);
+			// Busco los vuelos que se vuelan con la aeronave "actual" (vuelo.getAeronave())
+			ArrayList<Vuelo> vuelosActual = buscarVuelosAeronave(vuelos, vuelo.getAeronave());
+			// Comparo el tamaño de las dos listas
+			if (vuelosActual.size() > vuelosMayor.size()) { 
+				mayor = vuelo.getAeronave();
+			}
+		}
+		
+		return mayor;
+	}
 
 	/*
 	 * mostrarEstadisticas: método estático que recibe una lista de vuelos y muestra por pantalla
@@ -91,21 +120,50 @@ public class DeustoAir {
 	 * - Aeronave W: 2 vuelos.
 	 * ...
 	 */
-
+	public static void mostrarEstadisticas(ArrayList<Vuelo> vuelos) {
+		// Creamos una lista con todas las aeronaves (no repetidas)
+		ArrayList<Aeronave> aeronaves = new ArrayList<Aeronave>();
+		
+		for (Vuelo vuelo : vuelos) {
+			// Si la aeronave de este vuelo no está contenida en la lista, la añadimos
+			if (!aeronaves.contains(vuelo.getAeronave())) {
+				aeronaves.add(vuelo.getAeronave());
+			}
+		}
+		
+		for (Aeronave aeronave : aeronaves) {
+			// Preguntamos cuántos vuelos se vuelan con esta aeronave
+			ArrayList<Vuelo> vuelosDeEstaAeronave = buscarVuelosAeronave(vuelos, aeronave);
+			// Mostramos la información
+			System.out.println(aeronave + ": " + vuelosDeEstaAeronave.size() + " vuelos.");
+		}
+	}
+	
 	
 	public static void main(String[] args) {
 		ArrayList<Aeronave> flota = new ArrayList<Aeronave>();
 
-		flota.add(new Avioneta(226, 1289, 4, 450));   // similar a Cessna 172
-		flota.add(new Avioneta(315, 1800, 6, 700));   // similar a Piper PA-32
-		flota.add(new Helicoptero(260, 640, 6, 1200, 9));   // helicóptero ligero de rescate
-		flota.add(new Helicoptero(287, 857, 12, 4000, 7));  // helicóptero medio de transporte
-		flota.add(new Helice(667, 1500, 72, 2, 2750, 1200));   // similar a ATR 72
-		flota.add(new Helice(540, 2000, 50, 2, 2500, 1100));   // similar a Dash 8
-		flota.add(new Reaccion(840, 6100, 180, 2, 120000, 12000));  // similar a Boeing 737
-		flota.add(new Reaccion(828, 5700, 150, 2, 110000, 12000));  // similar a Airbus A320
-		flota.add(new Reaccion(900, 15000, 300, 4, 280000, 13000)); // similar a Boeing 747
-		flota.add(new Reaccion(905, 14000, 250, 2, 320000, 13000)); // similar a Boeing 787
+		Avioneta avioneta1 = new Avioneta(226, 1289, 4, 450);   // similar a Cessna 172
+		Avioneta avioneta2 = new Avioneta(315, 1800, 6, 700);   // similar a Piper PA-32
+		Helicoptero helicoptero1 = new Helicoptero(260, 640, 6, 1200, 9);   // helicóptero ligero de rescate
+		Helicoptero helicoptero2 = new Helicoptero(287, 857, 12, 4000, 7);  // helicóptero medio de transporte
+		Helice helice1 = new Helice(667, 1500, 72, 2, 2750, 1200);   // similar a ATR 72
+		Helice helice2 = new Helice(540, 2000, 50, 2, 2500, 1100);   // similar a Dash 8
+		Reaccion reaccion1 = new Reaccion(840, 6100, 180, 2, 120000, 12000);  // similar a Boeing 737
+		Reaccion reaccion2 = new Reaccion(828, 5700, 150, 2, 110000, 12000);  // similar a Airbus A320
+		Reaccion reaccion3 = new Reaccion(900, 15000, 300, 4, 280000, 13000); // similar a Boeing 747
+		Reaccion reaccion4 = new Reaccion(905, 14000, 250, 2, 320000, 13000); // similar a Boeing 787
+		
+		flota.add(avioneta1);
+		flota.add(avioneta2);
+		flota.add(helicoptero1);
+		flota.add(helicoptero2);
+		flota.add(helice1);
+		flota.add(helice2);
+		flota.add(reaccion1);
+		flota.add(reaccion2);
+		flota.add(reaccion3);
+		flota.add(reaccion4);
 		
 		ArrayList<Pasajero> pasajeros = new ArrayList<>();
 
@@ -132,12 +190,13 @@ public class DeustoAir {
 		
 		ArrayList<Aeropuerto> aeropuertos = new ArrayList<>();
 
+		Aeropuerto bilbao = new Aeropuerto(43.3011, -2.9106, "BIO", 2,
+		        new SalaVIP(true, 20, new ArrayList<Pasajero>())); // Bilbao
+		aeropuertos.add(bilbao);
 		aeropuertos.add(new Aeropuerto(40.4722, -3.5608, "MAD", 4,
 		        new SalaVIP(true, 20, new ArrayList<Pasajero>()))); // Madrid-Barajas
 		aeropuertos.add(new Aeropuerto(41.2974, 2.0833, "BCN", 3,
 		        new SalaVIP(true, 20, new ArrayList<Pasajero>()))); // Barcelona-El Prat
-		aeropuertos.add(new Aeropuerto(43.3011, -2.9106, "BIO", 2,
-		        new SalaVIP(true, 20, new ArrayList<Pasajero>()))); // Bilbao
 		aeropuertos.add(new Aeropuerto(36.6749, -4.4991, "AGP", 2,
 		        new SalaVIP(true, 20, new ArrayList<Pasajero>()))); // Málaga
 		aeropuertos.add(new Aeropuerto(39.4893, -0.4816, "VLC", 2,
@@ -175,8 +234,20 @@ public class DeustoAir {
 		
 		ArrayList<Vuelo> vuelos = generarVuelos(flota, pasajeros, aeropuertos);
 		
+		System.out.println("Mostrando todos los vuelos...");
 		System.out.println(vuelos);
 		
+		System.out.println("Mostrando todos los vuelos desde Bilbao...");
+		System.out.println(buscarVuelosOrigen(vuelos, bilbao));
+		
+		System.out.println("Mostrando todos los vuelos con la avioneta1...");
+		System.out.println(buscarVuelosAeronave(vuelos, avioneta1));
+		
+		System.out.println("Mostrando la aeronave más utilizada...");
+		System.out.println(mayorAeronave(vuelos));
+		
+		System.out.println("Mostrando estadísticas...");
+		mostrarEstadisticas(vuelos);
 		
 	}
 
